15d9dbf325424dc1fd6b7b506f13485ac94fc68e
Nikhil Rajpal <nikrdc@gmail.com>
2015-05-20 19:11:20 -0400

    started home page template

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'substitute key'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+                            'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
